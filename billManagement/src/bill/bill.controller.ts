4598170bd43a84eaf7e268fb6af18a5691c0a226
ELmessoudizakaria <js.developer02@gmail.com>
2019-07-22 15:58:35 +0200

    updating structure

--- billManagement/src/bill/bill.controller.ts
+++ billManagement/src/bill/bill.controller.ts
@@ -1,26 +1,31 @@
-import { Controller, Body, Post, Param, Get } from "@nestjs/common";
-import { BillService } from "./bill.service";
-import { CreateBillDto } from "./dto/bill.dto";
-import { Bill } from "./interfaces/bill.interface";
-
+import { Controller, Body, Post, Param, Get } from '@nestjs/common';
+import { BillService } from './bill.service';
+import { CreateBillDto } from '../dto/bill.dto';
+import { Bill } from '../interfaces/bill.interface';
 
 @Controller('bill')
-export class BillController{
-    constructor(private readonly billService:BillService){}
+export class BillController {
+    constructor(private readonly billService: BillService) {}
 
     @Post()
-    async create(@Body() createBillDto:CreateBillDto):Promise<Bill>{
+    async create(@Body() createBillDto: CreateBillDto): Promise<Bill> {
         return await this.billService.create(createBillDto);
     }
 
     @Get('contrat/:id')
-    async getBillsByContrat(@Param('id') contratId:string):Promise<Bill[]>{
+    async getBillsByContrat(@Param('id') contratId: string): Promise<Bill[]> {
         return await this.billService.findByContrat(contratId);
     }
 
     @Get(':id/:month')
-    async getBillsInLastXMonthByContrat(@Param('id') contratId:string,@Param('month') months:string):Promise<Bill[]>{
+    async getBillsInLastXMonthByContrat(
+        @Param('id') contratId: string,
+        @Param('month') months: string
+    ): Promise<Bill[]> {
         const month = parseInt(months);
-        return await this.billService.findByContratAndLastXMonth(contratId,month);
+        return await this.billService.findByContratAndLastXMonth(
+            contratId,
+            month
+        );
     }
 }
--- billManagement/src/bill/bill.service.ts
+++ billManagement/src/bill/bill.service.ts
@@ -1,28 +1,27 @@
-import { Injectable } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
-import { Bill } from "./interfaces/bill.interface";
-import { CreateBillDto } from "./dto/bill.dto";
-
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Bill } from '../interfaces/bill.interface';
+import { CreateBillDto } from '../dto/bill.dto';
 
 @Injectable()
-export class BillService{
-    constructor(@InjectModel('Bill') private readonly billModel:Model<Bill>){}
+export class BillService {
+    constructor(@InjectModel('Bill') private readonly billModel: Model<Bill>) {}
 
-    async create(createBillDto:CreateBillDto):Promise<Bill>{
+    async create(createBillDto: CreateBillDto): Promise<Bill> {
         const createdBill = new this.billModel(createBillDto);
         return await createdBill.save();
     }
 
-    async findByContrat(contratId:string):Promise<Bill[]>{
-        const filter = {contratId:contratId};
-       return await this.billModel.find(filter).exec();
+    async findByContrat(contratId: string): Promise<Bill[]> {
+        const filter = { contratId: contratId };
+        return await this.billModel.find(filter).exec();
     }
 
-    async findByContratAndLastXMonth(contratId:string, months:number){
+    async findByContratAndLastXMonth(contratId: string, months: number) {
         let date = new Date();
-            date.setMonth(date.getMonth() - months);
-        const filter={contratId:contratId,dateCreation:{$gte:date}};
+        date.setMonth(date.getMonth() - months);
+        const filter = { contratId: contratId, dateCreation: { $gte: date } };
         return await this.billModel.find(filter).exec();
     }
 }
